9e8e878476064629488809867b8d60e7db463d3f
Juan De Leon <juan.deleon@bigcommerce.com>
2022-06-06 12:35:30 -0500

    feat(IL-124): Implement role creation

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -2,4 +2,24 @@ class RolesController < ApplicationController
   def index
     render json: Role.all
   end
+
+  def create
+    # TODO: Implement validation to ensure only admin users can add new roles
+    begin
+      role = Role.new(role_params)
+      if role.save
+        render json: role, status: :created
+      else
+        render json: role.errors, status: :unprocessable_entity
+      end
+    rescue StandardError => e
+      Rails.logger.error("Error while creating new role: #{e.message}", e)
+    end
+  end
+end
+
+private
+
+def role_params
+  params.require(:role).permit(:name)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   resources :actions, only: :index
-  resources :views, only: :index
   resources :users
+  resources :roles, only: :create
+  resources :views, only: :index
 
 end
